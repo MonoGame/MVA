889a96d8649ffbe7dcd551f6d539e64ef32a01af
Andy Dunn <zman@thezbuffer.com>
2014-12-03 18:19:23 -0800

    Universal Project that doesn't work

---
+++ Module4/Wumpus/Wumpus/Wumpus.Shared/GamePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MonoGame.Framework;
+
+namespace Wumpus
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage : SwapChainBackgroundPanel
+    {
+        readonly Game1 _game;
+
+        public GamePage()
+        {
+            this.InitializeComponent();
+        }
+
+        public GamePage(string launchArguments)
+        {
+            _game = XamlGame<Game1>.Create(launchArguments, Window.Current.CoreWindow, this);
+        }
+    }
+}
